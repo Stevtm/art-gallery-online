5c335f1aa38aa84b3a87c8189d33f3bdd61a83aa
Jade Jhagru <j.jhagru@hotmail.com>
2021-07-21 14:29:13 -0400

    added first image to gallery

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import MyNavbar from "./components/navbar/navbar.component";
 import ArtCarousel from "./components/carousel/carousel.component";
+import ArtGallery from "./pages/gallery/gallery";
+import Fade from "react-reveal/Fade"
+import Container from "react-bootstrap/Container";
 import "./App.css";
 
 const App = () => {
@@ -8,8 +11,15 @@ const App = () => {
     <div className="App" style={{ position: "relative" }}>
       <MyNavbar />
       <ArtCarousel />
-      </div>
 
+      <div>
+        <Container className="container-box rounded">
+          <Fade duration={500}>
+            <ArtGallery />
+          </Fade>
+        </Container>
+      </div>
+    </div>
   );
 };
 
