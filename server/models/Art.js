0e60ebf9fadf5c98632f1c3220c89e687e461b54
Stevtm <steventmaddison@outlook.com>
2021-07-21 19:36:24 -0400

    models review pt1

--- server/models/Art.js
+++ server/models/Art.js
@@ -1,36 +1,46 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const ArtSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 100,
-  },
-  img: {
-    data: Buffer,
-    contentType: String
-  },
-  category: {
-    type: String,
-    required: ['photograph', 'drawing', 'painting'],
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  tag: {
-    type: String,
-  },
-  comments: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Comment'
-    }
-  ],
+	// do we want a custom error message for the back-end? I'm assuming we'll manage errors in form submission on the front end
+	title: {
+		type: String,
+		required: true,
+		minlength: 1,
+		maxlength: 100,
+	},
+	description: {
+		type: String,
+		required: true,
+		minlength: 1,
+		maxlength: 100,
+	},
+	img: {
+		data: Buffer,
+		contentType: String,
+	},
+	category: {
+		type: String,
+		required: ["photograph", "drawing", "painting"],
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+	tag: [
+		{
+			type: String,
+		},
+	],
+	comments: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "Comment",
+		},
+	],
+	// likes? User?
 });
 
-const Art = model('Art', ArtSchema);
+const Art = model("Art", ArtSchema);
 
 module.exports = Art;
--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,26 +1,25 @@
-const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const CommentSchema = new Schema (
-    {
-        commentText: {
-            type: String,
-            required: 'You need to enter a comment!',
-            minlength: 1,
-            maxlength: 240
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: timestamp => dateFormat(timestamp)
-        },
-        username: {
-            type: String,
-            required: true
-        }
-    }
-);
+const CommentSchema = new Schema({
+	commentText: {
+		type: String,
+		required: "You need to enter a comment!",
+		minlength: 1,
+		maxlength: 240,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+		get: (timestamp) => dateFormat(timestamp),
+	},
+	// ref to user model?
+	username: {
+		type: String,
+		required: true,
+	},
+});
 
-const Comment = model('Comment', CommentSchema);
+const Comment = model("Comment", CommentSchema);
 
 module.exports = Comment;
