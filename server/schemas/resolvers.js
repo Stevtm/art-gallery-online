54ca69bfca5dd09a4ffaf7f085562400d858a886
Stevtm <steventmaddison@outlook.com>
2021-07-21 20:59:12 -0400

    push recent formatting

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,13 +3,11 @@ const { User, Art, Comment } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
-<<<<<<< HEAD
 	Query: {
-		me: async (context) => {
+		me: async (parent, args, context) => {
 			if (context.user) {
 				const userData = await User.findOne({ _id: context.user._id })
-					// no more pw in the db
-					.select("-__v, -password")
+					.select("-__v")
 					.populate("art");
 
 				return userData;
@@ -18,103 +16,48 @@ const resolvers = {
 			throw new AuthenticationError("Not logged in");
 		},
 		users: async () => {
-			// no more pw in the db
-			return User.find().select("-__v, -password").populate("art");
+			return User.find().select("-__v").populate("art");
 		},
-		user: async ({ username }) => {
-			return (
-				User.findOne({ username })
-					// no more pw in the db
-					.select("-__v -password")
-					.populate("art")
-			);
+		user: async (parent, { username }) => {
+			return User.findOne({ username }).select("-__v").populate("art");
 		},
-		art: async ({ title, category, price, tag }) => {
+		art: async (parent, { title, category, price, tag }) => {
 			return Art.findOne({ title, category, price, tag })
 				.select("-__v")
-				.populate("comments, likes");
+				.populate("comments", "likes");
 		},
-		comments: async ({ username }) => {
+		comments: async (parent, { username }) => {
 			const params = username ? { username } : {};
 			return Comment.find(params).sort({ createdAt: -1 });
 		},
 	},
 
 	Mutation: {
-		addUser: async (args) => {
-			const user = await User.create(args);
+		addUser: async (parent, { args }) => {
+			const { username, email } = args;
+
+			const user = await User.create(username, email);
 			const token = signToken(user);
 
 			return { token, user };
 		},
-		login: async ({ email, password }) => {
+		login: async (parent, { email, password }) => {
 			const user = await User.findOne({ email });
 
 			if (!user) {
 				throw new AuthenticationError("Incorrect credentials.");
 			}
 
-			const correctPw = await user.isCorrectPassword(password);
+			// const correctPw = await user.isCorrectPassword(password);
 
-			if (!correctPw) {
-				throw new AuthenticationError("Incorrect credentials.");
-			}
+			// if (!correctPw) {
+			// 	throw new AuthenticationError("Incorrect credentials.");
+			// }
 
 			const token = signToken(user);
 			return { token, user };
 		},
 	},
-=======
-  Query: {
-    me: async (parent, args, context) => {
-      if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-          .select('-__v')
-          .populate('art');
-
-        return userData;
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    users: async () => {
-      return User.find().select('-__v').populate('art');
-    },
-    user: async (parent, { username }) => {
-      return User.findOne({ username })
-        .select('-__v')
-        .populate('art');
-    },
-    art: async (parent, { title, category, price, tag }) => {
-      return Art.findOne({ title, category, price, tag })
-        .select('-__v')
-        .populate('comments, likes');
-    },
-    comments: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Comment.find(params).sort({ createdAt: -1 });
-    },
-  },
-
-  Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
-
-      return { token, user };
-    },
-    login: async (parent, { email }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('Incorrect credentials.');
-      }
-
-      const token = signToken(user);
-      return { token, user };
-    },
-  },
->>>>>>> b6672ed2940d70a0431d0fb562871af2246b1fe9
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,26 +1,21 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-<<<<<<< HEAD
 	type User {
 		_id: ID
 		username: String
 		email: String
 		art: [Art]
-		# comments: [Comment]
 	}
 
 	type Art {
 		_id: ID
 		title: String
-		# description: String
 		img: String
 		category: String
 		price: Int
 		tag: String
-		# likes: [Like]
 		comments: [Comment]
-		# user: [user]
 	}
 
 	type Comment {
@@ -46,7 +41,6 @@ const typeDefs = gql`
 		users: [User]
 		user(username: String!): User
 		art(title: String, category: String, price: Int, tag: String): Art
-		# when would we use these comments and likes queries?
 		comments(username: String!): Comment
 		likes: [Art]
 	}
@@ -54,7 +48,6 @@ const typeDefs = gql`
 	type Mutation {
 		login(email: String!, password: String!): Auth
 		addUser(username: String!, email: String!, password: String!): Auth
-		# some categories missing, but I'm assuming it's just for testing?
 		addArt(
 			username: String!
 			title: String!
@@ -64,64 +57,6 @@ const typeDefs = gql`
 		addComment(username: String!, commentText: String!): Art
 		addLike(username: String!): Art
 	}
-=======
-  type User {
-    _id: ID
-    username: String
-    email: String
-    art: [Art]
-  }
-
-  type Art {
-    _id: ID
-    title: String
-    img: String
-    category: String
-    price: Int
-    tag: String
-    comments: [Comment]
-  }
-
-  type Comment {
-    _id: ID
-    commentText: String
-    createdAt: String
-    username: String
-  }
-
-  type Like {
-    _id: ID
-    username: String
-    like: Int
-  }
-
-  type Auth {
-    token: ID!
-    user: User
-  }
-
-  type Query {
-    me: User
-    users: [User]
-    user(username: String!): User
-    art(title: String, category: String, price: Int, tag: String): Art
-    comments(username: String!): Comment
-    likes: [Art]
-  }
-
-  type Mutation {
-    login(email: String!): Auth
-    addUser(username: String!, email: String!): Auth
-    addArt(
-      username: String!
-      title: String!
-      category: String!
-      price: Int!
-    ): User
-    addComment(username: String!, commentText: String!): Art
-    addLike(username: String!): Art
-  }
->>>>>>> b6672ed2940d70a0431d0fb562871af2246b1fe9
 `;
 
 module.exports = typeDefs;
