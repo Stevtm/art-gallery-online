37465fff6b8fb572e4aaf1ad58c785ffc134bc13
Stevtm <steventmaddison@outlook.com>
2021-07-24 20:35:34 -0400

    add firebase config and install firebase

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,63 +1,63 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type User {
-    _id: ID
-    username: String
-    email: String
-    art: [Art]
-  }
+	type User {
+		_id: ID
+		username: String
+		email: String
+		art: [Art]
+	}
 
-  type Art {
-    _id: ID
-    title: String
-    img: String
-    category: String
-    description: String
-    price: Int
-    tag: [String]
-    comments: [Comment]
-    likeCount: Int
-  }
+	type Art {
+		_id: ID
+		title: String
+		img: String
+		category: String
+		description: String
+		price: Int
+		tag: [String]
+		comments: [Comment]
+		likeCount: Int
+	}
 
-  type Comment {
-    _id: ID
-    commentText: String
-    createdAt: String
-    username: String
-  }
+	type Comment {
+		_id: ID
+		commentText: String
+		createdAt: String
+		username: String
+	}
 
-  type Like {
-    username: String!
-  }
+	type Like {
+		username: String!
+	}
 
-  type Auth {
-    token: ID!
-    user: User
-  }
+	type Auth {
+		token: ID!
+		user: User
+	}
 
-  type Query {
-    me: User
-    users: [User]
-    user(username: String!): User
-    art: [Art]
-    comments(username: String!): Comment
-    likes: [Art]
-  }
+	type Query {
+		me: User
+		users: [User]
+		user(username: String!): User
+		art: [Art]
+		comments(username: String!): Comment
+		likes: [Art]
+	}
 
-  type Mutation {
-    login(email: String!): Auth
-    addUser(username: String!, email: String!): Auth
-    addArt(
-      username: String!
-      title: String!
-      category: String!
-      price: Int!
-      description: String!
-    ): Art
-    addComment(artId: ID!, username: String!, commentText: String!): Art
-    addLike(artId: ID!, username: String!): Art
-  }
+	type Mutation {
+		login(email: String!, password: String!): Auth
+		addUser(username: String!, email: String!, password: String!): Auth
+		addArt(
+			username: String!
+			title: String!
+			category: String!
+			price: Int!
+			description: String!
+		): Art
+		addComment(artId: ID!, username: String!, commentText: String!): Art
+		addLike(artId: ID!, username: String!): Art
+	}
 `;
 
 module.exports = typeDefs;
