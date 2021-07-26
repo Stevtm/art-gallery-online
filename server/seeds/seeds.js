077f2e95a5390e39af343ae26d6733892a262a24
powerar <arpower@bell.net>
2021-07-26 14:53:35 -0400

    added arts, users, comments, and likes seeds

--- server/seeds/seeds.js
+++ server/seeds/seeds.js
@@ -4,42 +4,85 @@ const db = require('../config/connection');
 const { Art, User, Comment, Like } = require('../models');
 
 db.once('open', async () => {
-    // create user data
-    const userData = [];
+  await User.deleteMany({});
+  await Art.deleteMany({});
+  // create user data
+  const userData = [];
 
-    for (let i = 0; i < 50; i += 1) {
-        const username = faker.internet.userName();
-        const email = faker.internet.email(username);
-        const password = faker.internet.password();
+  for (let i = 0; i < 50; i += 1) {
+    const username = faker.internet.userName();
+    const email = faker.internet.email(username);
+    const password = faker.internet.password();
 
-        userData.push({ username, email, password });
-    }
+    userData.push({ username, email, password });
+  }
 
-    const createdUsers = await User.collection.insertMany(userData);
+  const createdUsers = await User.collection.insertMany(userData);
 
-    // create Art
-    let createdArts = [];
-    for (let i = 0; i < 100; i += 1) {
-        const title = faker.lorem.words(Math.round(Math.random() * 10 ) + 1);
-        const description = faker.lorem.words(Math.round(Math.random() * 10) + 1);
-        const categories = ['photograph', 'drawing', 'painting'];
-        
-        const category = Math.floor(Math.random() * categories.length);
-        const price = Math.round(Math.random() * 1000);
+  // create Art
+  let createdArts = [];
+  for (let i = 0; i < 100; i += 1) {
+    const title = faker.lorem.words(Math.round(Math.random() * 10) + 1);
+    const description = faker.lorem.words(Math.round(Math.random() * 10) + 1);
+    const categories = ['photograph', 'drawing', 'painting'];
 
-        const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-        const { _id: userId } = createdUsers.ops[randomUserIndex];
+    const category = Math.floor(Math.random() * categories.length);
+    const price = Math.round(Math.random() * 1000);
 
-        const createdArt = await Art.create({ title, description, category, price });
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { _id: userId } = createdUsers.ops[randomUserIndex];
 
-        const updatedUser = await User.updateOne(
-            { _id: userId },
-            { $push: { art: createdArt._id }}
-        );
+    const createdArt = await Art.create({
+      title,
+      description,
+      category,
+      price,
+    });
 
-        createdArts.push(createdArt);
-    }
+    const updatedUser = await User.updateOne(
+      { _id: userId },
+      { $push: { art: createdArt._id } }
+    );
 
-    console.log('Finished!');
-    process.exit(0);
+    createdArts.push(createdArt);
+  }
+
+  //create Comments
+  for (let i = 0; i < 100; i += 1) {
+    let commentText = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { username } = createdUsers.ops[randomUserIndex];
+
+    const randomArtIndex = Math.floor(Math.random() * createdArts.length);
+    const { _id: artId } = createdArts[randomArtIndex];
+
+    console.log(artId);
+    console.log(commentText);
+    console.log(username);
+
+    console.log(await Art.updateOne(
+      { _id: artId },
+      { $addToSet: { comments: { commentText, username } } },
+      { runValidators: true }
+    ));
+  }
+
+  //create likes
+  for (let i = 0; i < 100; i += 1) {
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { username } = createdUsers.ops[randomUserIndex];
+
+    const randomArtIndex = Math.floor(Math.random() * createdArts.length);
+    const { _id: artId } = createdArts[randomArtIndex];
+
+    await Art.updateOne(
+      { _id: artId },
+      { $addToSet: { likes: { username } } },
+      { runValidators: true }
+    );
+  }
+
+  console.log('Finished!');
+  process.exit(0);
 });
